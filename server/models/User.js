9ab4db7378079d94243aea8d8f6bba4c8473d7da
Mertnode <mertnode@gmail.com>
2023-11-15 23:54:47 +0300

    sifre hashleme yapildi

--- server/models/User.js
+++ server/models/User.js
@@ -89,5 +89,14 @@ const userSchema = Schema({
     } 
 
 })
+// Encrypt password before saving to DB
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+});
 export default mongoose.model("users", userSchema);
